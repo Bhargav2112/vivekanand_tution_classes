29694b9a67fe2d304ed9578d9c053aa2f752d2d0
Bhargav2112 <bsparmar1221@gmail.com>
2026-08-06 08:34:15 +0530

    Scale CustomIcons globally by 30% for better UI visibility

--- vivekanand-user/src/components/ui/CustomIcons.jsx
+++ vivekanand-user/src/components/ui/CustomIcons.jsx
@@ -1,9 +1,10 @@
 import { cn } from "@/lib/utils";
 
 export const CallIcon = ({ className }) => (
-  <img src="/call.png" alt="Call" className={cn("object-contain", className)} />
+  <img src="/call.png" alt="Call" className={cn("object-contain scale-[1.3]", className)} />
 );
 
 export const WhatsAppIcon = ({ className }) => (
-  <img src="/whatsapp.png" alt="WhatsApp" className={cn("object-contain", className)} />
+  <img src="/whatsapp.png" alt="WhatsApp" className={cn("object-contain scale-[1.3]", className)} />
 );
+
